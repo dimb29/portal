3aecc2b8ad9ec04b5f03664702801adae0c1db5b
dimbas29 <dimasbagas321@gmail.com>
2023-02-21 13:50:54 +0700

    add

--- config/filesystems.php
+++ config/filesystems.php
@@ -30,9 +30,15 @@ return [
 
     'disks' => [
 
+        // 'local' => [
+        //     'driver' => 'local',
+        //     'root' => storage_path('app'),
+        // ],
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('app'),
+            'root' => storage_path('app/public'),
+            'url' => env('APP_URL').'/storage',
+            'visibility' => 'public',
         ],
 
         'public' => [
